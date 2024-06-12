bff3335f87662063a39a9cc026936c8dbdcc3124
scott <scottysora@gmail.com>
2024-06-12 00:47:22 -0400

    better naming variables

--- exercises/users-fetch-table/scripts/edit.js
+++ exercises/users-fetch-table/scripts/edit.js
@@ -1,13 +1,13 @@
 async function editTableOfUsers() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const userId = urlParams.get("id");
+  let urlParams = new URLSearchParams(window.location.search);
+  let userId = urlParams.get("id");
   await fetchUserData(userId);
 }
 
 async function fetchUserData(userId) {
   try {
-    const response = await fetch(`http://localhost:3000/users/${userId}`);
-    const user = await response.json();
+    let response = await fetch(`http://localhost:3000/users/${userId}`);
+    let user = await response.json();
     document.querySelector("#name").value = user.name;
     document.querySelector("#username").value = user.username;
     document.querySelector("#email").value = user.email;
@@ -18,27 +18,27 @@ async function fetchUserData(userId) {
 
 async function editUserData() {
   try {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userId = urlParams.get("id");
+    let urlParams = new URLSearchParams(window.location.search);
+    let userId = urlParams.get("id");
 
-    const name = document.querySelector("#name").value;
-    const username = document.querySelector("#username").value;
-    const email = document.querySelector("#email").value;
+    let name = document.querySelector("#name").value;
+    let username = document.querySelector("#username").value;
+    let email = document.querySelector("#email").value;
 
-    const userData = {
+    let userData = {
       name: name,
       username: username,
       email: email,
     };
 
-    const response = await fetch(`http://localhost:3000/users/${userId}`, {
+    let response = await fetch(`http://localhost:3000/users/${userId}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(userData),
     });
-    const data = await response.json();
+    let data = await response.json();
     console.log("User updated:", data);
     window.location.href = "index.html";
   } catch (error) {
@@ -48,10 +48,10 @@ async function editUserData() {
 
 editTableOfUsers();
 
-// fetch() & .then solution
+// fetch() & .then
 // function editTableOfUsers() {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const userId = urlParams.get("id");
+//   let urlParams = new URLSearchParams(window.location.search);
+//   let userId = urlParams.get("id");
 //   fetchUserData(userId);
 // }
 
@@ -69,14 +69,14 @@ editTableOfUsers();
 // }
 
 // function editUserData() {
-//   const urlParams = new URLSearchParams(window.location.search);
-//   const userId = urlParams.get("id");
+//   let urlParams = new URLSearchParams(window.location.search);
+//   let userId = urlParams.get("id");
 
-//   const name = document.querySelector("#name").value;
-//   const username = document.querySelector("#username").value;
-//   const email = document.querySelector("#email").value;
+//   let name = document.querySelector("#name").value;
+//   let username = document.querySelector("#username").value;
+//   let email = document.querySelector("#email").value;
 
-//   const userData = {
+//   let userData = {
 //     name: name,
 //     username: username,
 //     email: email,
